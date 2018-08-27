9c7bc0b6b8f64fdaaa44c0ddf9d60eb4020a49b8
August Duet <august.duet@revature.com>
2018-08-27 14:17:41 -0400

    pray that this works

---
+++ src/main/java/com/ex/web/BookServlet.java
@@ -0,0 +1,25 @@
+package com.ex.web;
+
+import com.ex.model.Book;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class BookServlet extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Book b = new Book();
+        b.setTitle("Web Programming for Dummies");
+        b.setAuthor("August Duet");
+        b.setPubYear("2020");
+
+        String bJson = new ObjectMapper().writeValueAsString(b);
+        resp.getWriter().write(bJson);
+        resp.setStatus(200);
+        resp.setHeader("Content-Type", "application/json");
+    }
+}
